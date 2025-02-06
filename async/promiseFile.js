8338b6d1ff48fae7b7d781259be8cc6d5d42b6fd
Pavan-personal <kumarpavan39877@gmail.com>
2025-02-06 16:40:34 +0530

    promise architecture finished

--- async/promiseFile.js
+++ async/promiseFile.js
@@ -4,21 +4,28 @@
 
 const random = () => {};
 
-const p = new Promise(function (InnerParameterFunction) {
-//   InnerParameterFunction();
-//   console.log("Parameter function of promise");
+const p = new Promise(function (
+  InnerParameterFunction1,
+  InnerParameterFunction2
+) {
+  //   InnerParameterFunction();
+  InnerParameterFunction2();
+  //   console.log("Parameter function of promise");
 });
 
 // console.log(p); // Promise { <pending> }
 let i = 1;
 p.then(() => {
-    // i = 2;
-    // change i to 2
-    i = 2;
-    console.log("here")
-    // console.log("This needs to be printed finally")
-})
+  // i = 2;
+  // change i to 2
+  i = 2;
+  //   console.log("here");
+  console.log("InnerParameterFunction1 is called");
+  // console.log("This needs to be printed finally")
+}).catch(() => {
+  console.log("InnerParameterFunction2 is called");
+});
 
-console.log(i); // 1
+// console.log(i); // 1
 
-
+// Promise takes an function as an argumnet, and that function's first argument is a function, which is used to resolve the promise and other is used to reject the promise
